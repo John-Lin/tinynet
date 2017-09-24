df180f6a5932c8f136c6c041fa1175459a578276
John-Lin <linton.tw@gmail.com>
2017-09-25 01:19:50 +0800

    add a example for creating a switch with 8 hosts

--- examples/topo-single-8.go
+++ examples/topo-single-8.go
@@ -19,16 +19,17 @@ func main() {
 	// add 8 IPs with CIDR 192.168.1.0/24
 	ips, _ := tn.GetIPs("192.168.1.0/24", 8)
 
+	var host[8] *tn.Host
 	// add 8 hosts and link to switch
 	for (i := 0; i < 7; i++) {
 		hostName := "h" + strconv.Itoa(i)
-		tn.AddHost(hostName, ips[i])
+		host[i], err = tn.AddHost(hostName, ips[i] + "/24")
 		if err != nil {
 			log.Fatal("failed to add host:", err)
 		}
 
 		// add Link for leftHost - leftSwitch
-		if err := tn.AddLink(hostName, Switch); err != nil {
+		if err := tn.AddLink(host[i], Switch); err != nil {
 			log.Fatal("failed to add link between Switch and host: ", err)
 		}
 	}
