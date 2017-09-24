23c3733e31232892ea890ec3e3b9f7e777c6ef52
John-Lin <linton.tw@gmail.com>
2017-09-25 01:38:36 +0800

    fixed example

--- examples/topo-single-8.go
+++ examples/topo-single-8.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"strconv"
+
 	tn "github.com/John-Lin/tinynet"
 	log "github.com/sirupsen/logrus"
-	"strconv"
 )
 
 // Custom topology example
@@ -19,16 +20,16 @@ func main() {
 	// add 8 IPs with CIDR 192.168.1.0/24
 	ips, _ := tn.GetIPs("192.168.1.0/24", 8)
 
-	var host[8] *tn.Host
+	var host [8]*tn.Host
 	// add 8 hosts and link to switch
-	for (i := 0; i < 7; i++) {
+	for i := 0; i < 8; i++ {
 		hostName := "h" + strconv.Itoa(i)
-		host[i], err = tn.AddHost(hostName, ips[i] + "/24")
+		host[i], err = tn.AddHost(hostName, ips[i]+"/24")
 		if err != nil {
 			log.Fatal("failed to add host:", err)
 		}
 
-		// add Link for leftHost - leftSwitch
+		// add Link for host[i] to Switch
 		if err := tn.AddLink(host[i], Switch); err != nil {
 			log.Fatal("failed to add link between Switch and host: ", err)
 		}
